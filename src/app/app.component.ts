c3ddd99c97a5bbcb543e052f21ce6443b9d53fad
Dasha Yarkovska <d.yarkovska@unkd.com.ua>
2023-06-15 15:36:02 +0300

    add restriction for cyrillic letters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,17 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   myForm: FormGroup = new FormGroup({
-    password: new FormControl(''),
+    password: new FormControl('', Validators.pattern(/^[^а-я]*$/i)),
   });
 
   passwordCheck(): string {
     const p = this.myForm.value.password;
+    console.log(this.myForm);
 
     const easy = /^(?:\d+|[a-z]+|[^a-z\d]+)$/i;
     const medium = /^(?:[a-z\d]+|[^a-z]+|[^\d]+)$/i;
 
-    if (p === '') return 'grey';
+    if (p === '' || this.myForm.invalid) return 'grey';
     else if (p.match(easy)) return 'easy';
     else if (p.match(medium)) return 'medium';
     else return 'strong';
