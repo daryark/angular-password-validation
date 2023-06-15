846c31d1c524cddd7eb7e20d4d447e4fea559079
Dasha Yarkovska <d.yarkovska@unkd.com.ua>
2023-06-15 13:37:36 +0300

    refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,41 +11,19 @@ export class AppComponent {
     password: new FormControl(''),
   });
 
-  match(pass: string, reg: any) {
-    return pass.match(reg);
-  }
+  // match(p: string, reg: RegExp) {
+  //   return p.match(reg);
+  // }
 
   passwordCheck(): string {
-    const { match } = this;
-    const d = /^\d+$/;
-    const l = /^[a-zA-z]+$/i;
-    const dL = /^[A-Za-z\d]+$/;
+    const p = this.myForm.value.password;
 
-    const pass = this.myForm.value.password;
+    const easy = /^(?:\d+|[a-z]+|[^a-z\d]+)$/i;
+    const medium = /^(?:[a-z\d]+|[^a-z]+|[^/d]+)$/i;
 
-    if (match(pass, l) || match(pass, d) !== null) {
-      return 'easy';
-    } else if (match(pass, dL) !== null) {
-      return 'medium';
-    } else {
-      return 'grey';
-    }
-    switch (pass) {
-      // case password.match(/^[\d+]|[A-Za-z]+|[^\dA-Za-z]+$/):
-      //   console.log('e');
-      //   return 'easy';
-
-      // case password.match(/^[A-Za-z\d]+$/):
-      //   console.log('m');
-      //   return 'medium';
-
-      // case password.match(/^[A-Za-z\d/$%&?]+$/):
-      //   console.log(password.match(/^[A-Za-z\d/$%&?]+$/));
-      //   console.log('s');
-      //   return 'strong';
-
-      default:
-        return 'grey';
-    }
+    if (p === '') return 'grey';
+    else if (p.match(easy)) return 'easy';
+    else if (p.match(medium)) return 'medium';
+    else return 'strong';
   }
 }
