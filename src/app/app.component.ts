4215c9cd5eff698852600a40f090a86cab458a2f
Dasha Yarkovska <d.yarkovska@unkd.com.ua>
2023-06-16 10:31:24 +0300

    add under-eight symbols rule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,12 @@ export class AppComponent {
 
   passwordCheck(): string {
     const p = this.myForm.value.password;
-    console.log(this.myForm);
 
     const easy = /^(?:\d+|[a-z]+|[^a-z\d]+)$/i;
     const medium = /^(?:[a-z\d]+|[^a-z]+|[^\d]+)$/i;
 
     if (p === '' || this.myForm.invalid) return 'grey';
+    else if (p.length < 8) return 'less-eight';
     else if (p.match(easy)) return 'easy';
     else if (p.match(medium)) return 'medium';
     else return 'strong';
